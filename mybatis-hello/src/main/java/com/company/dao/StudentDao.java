8cabefab8a58cc98b1165629aad6ae1d8740806d
xiaosu <2257346739@qq.com>
2021-10-05 22:54:15 +0800

    Student持久层对象,查询Student所有数据

---
+++ mybatis-hello/src/main/java/com/company/dao/StudentDao.java
@@ -0,0 +1,17 @@
+package com.company.dao;
+
+/**
+ * @author 数据库持久层对象
+ * @version 1.0
+ * @ClassName StudentDao
+ * @company 公司
+ * @Description StudentDao
+ * @createTime 2021年10月05日 22:15:15
+ */
+public interface StudentDao {
+    /**
+     * 查询Student所有数据
+     * return selectStudents
+     */
+    void selectStudents();
+}
